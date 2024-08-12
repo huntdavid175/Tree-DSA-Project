18bde09af4c5028772e47e3f7d1e19e79d97a5b8
huntdavid175 <huntdavid175@gmail.com>
2024-08-12 15:57:35 -0400

    defined Parcel struct and hashtable struct and implemented function for generating keys for hashtable

--- main.c
+++ main.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
 
+#define TABLE_SIZE 127
+
+typedef struct Parcel
+{
+    char *destination;
+    int weight;
+    float value;
+    struct Parcel *leftChild;
+    struct Parcel *rightChild;
+} Parcel;
+
+typedef struct HashTable
+{
+    Parcel *table[TABLE_SIZE];
+} HashTable;
+
+int GenerateHash(char *key);
 int main(void)
 {
     return 1;
+}
+
+int GenerateHash(char *key)
+{
+    int hash = 0;
+    for (int i = 0; key[i] != '\0'; i++)
+    {
+        int asciiValue = key[i];
+        hash = (hash + asciiValue) % TABLE_SIZE;
+    }
+    return hash;
 }
